e45f97d5d812b967f02ef193e1d1c79f6660bbce
HoldCrowd <holdcrowdgame@gmail.com>
2026-09-09 21:38:45 +0300

    0 commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = 3000
+
+let countConnect = 0;
+
+//let ipServer = "127.0.0.1"// dev
+//let ipServer = "0.0.0.0"// prod
+
+app.get('/', (req, res) => {
+
+  res.send(JSON.stringify([
+    {
+      countConnect: countConnect,
+      ipServer: "127.0.0.1"// ipconfig 
+    }// Ethernet adapter Ethernet: // 2-й средний
+  ]))
+
+  countConnect++
+
+})
+
+app.listen(PORT, () => console.log(
+  `_ Express_for_Unity_Server started. Port ${PORT} _`))
